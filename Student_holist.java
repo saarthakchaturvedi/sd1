a636564770f722b5de6d817ab647fd2b341036a1
EmilioC1 <ec2736@princeton.edu>
2024-02-17 17:27:46 -0500

    testing smth

--- Student_holist.java
+++ Student_holist.java
@@ -16,26 +16,26 @@ public class Student_holist implements Student {
     private double quality;
 
     public int compareTo(School n) { // smaller pairs are higher quality
-      int ret = Double.compare(n.quality, quality);
+      int ret = Double.compare(quality, n.quality);
       return (ret == 0) ? (Integer.compare(index, n.index)) : ret;
     }
   }
+  // if n.quality is less than quality, returns -1
+  // if n.quality is greater than quality, returns 1
 
-  public int[] getApplications(
-      int N,
-      double S,
-      double T,
-      double W,
-      double aptitude,
-      List<Double> schools,
+  public int[] getApplications(int N, double S, double T, double W, double aptitude, List<Double> schools,
       List<Double> synergies) {
+
     School[] preferences = new School[schools.size()];
-    for (int i = 0; i != synergies.size(); ++i) {
-      preferences[i] = new School(i, schools.get(i) + synergies.get(i));
+    for (int i = 0; i < synergies.size(); i++) {
+      preferences[i] = new School(i,
+          (synergies.get(i) + schools.get(i)) *
+              ((aptitude + synergies.get(i)) / (S + W)));
     }
     Arrays.sort(preferences);
+    System.out.println("does this work??");
     int[] ret = new int[10];
-    for (int i = 0; i != 10; ++i) {
+    for (int i = 0; i < 10; i++) {
       ret[i] = preferences[i].index;
     }
     return ret;
