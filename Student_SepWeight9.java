c0fae015b8248d524ffd02f3eab06862df923c91
EmilioC1 <ec2736@princeton.edu>
2024-02-19 21:39:55 -0500

    trying somthe

--- Student_SepWeight9.java
+++ Student_SepWeight9.java
@@ -22,33 +22,38 @@ public class Student_SepWeight9 implements Student {
     public int[] getApplications(int N, double S, double T, double W, double aptitude, List<Double> schools,
             List<Double> synergies) {
         School[] preferences = new School[schools.size()];
+        School[] sortedByPayoff = new School[schools.size()];
 
         // TODO
         // doesn't take into account N, T, or the relative size of S/W and W/T
         // does take into account aptitude + synergy relative to other applicants for
         // each university.
-        double[] lambda = { .5, 1, 2, 4, 8
-        };
+        // double[] lambda = { .5, 1, 2, 4, 8};
         for (int i = 0; i < synergies.size(); i++) {
             double prob = ((aptitude + synergies.get(i)) / (W + S));
-            if (prob < .2)
-                prob *= lambda[0];
-            else if (prob < .4)
-                prob *= lambda[1];
-            else if (prob < .6)
-                prob *= lambda[2];
-            else if (prob < .8)
-                prob *= lambda[3];
-            else
-                prob *= lambda[4];
-            preferences[i] = new School(i,
-                    (prob) * (schools.get(i) + synergies.get(i)));
+
+            preferences[i] = new School(i, prob);
+
+            sortedByPayoff[i] = new School(i, schools.get(i) + synergies.get(i));
         }
         Arrays.sort(preferences);
+        Arrays.sort(sortedByPayoff);
 
         int[] ret = new int[10];
-        for (int i = 0; i < 10; i++) {
-            ret[i] = preferences[i].index;
+        ret[0] = preferences[0].index;
+        int guaranteedIndex = -1;
+        for (int i = 0; i < sortedByPayoff.length; i++) {
+            if (sortedByPayoff[i].index == ret[0]) {
+                guaranteedIndex = i;
+                break;
+            }
+        }
+        if (guaranteedIndex < 10) {
+            int[] ret2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            return ret2;
+        }
+        for (int i = 1; i < 10; i++) {
+            ret[i] = sortedByPayoff[guaranteedIndex - i].index;
         }
         return ret;
     }
